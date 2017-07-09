1a6a8ae74524417cf031a36f1dbaa969887a2ba6
Justin Mills <jbmills@gmail.com>
2017-07-09 14:05:47 -0700

    First draft of Crystals site

---
+++ routes.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+      root: './public/html'
+    });
+  });
+}
---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express'),
+  logger = require('morgan')('dev'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  routes = require('./routes');
+
+
+
+
+var PORT = process.env.PORT || 3000;
+
+
+var app = express();
+
+// mongoose.connect('mongodb://localhost/crystalBlog');
+
+app.use(express.static('public'));
+
+app.use(logger);
+
+app.post('*', bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.use(bodyParser.json());
+
+routes(app);
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("Server error", err);
+    process.exit(1);
+  }
+
+  console.log("Server is up and running on port", PORT);
+})
